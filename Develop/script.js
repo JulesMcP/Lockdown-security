5248f735927c0d7e10715f82d79f640e6145be68
JulesMcP <rogue427@yahoo.com>
2022-01-20 07:10:28 -0600

    changed character string to string array

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,39 @@
 // Assignment code here
+
 // GENERATE ELEMENT
 var generateBtn = document.querySelector("#generate");
 
-// GENERATOR FUNCTIONS FROM http://www.net-comber.com/charset.html
+//RANDOM ARRAY CHARACTERS IF CHOSEN
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["0", "9", "8", "7", "6", "5", "4", "3", "2", "1"];
+var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "~", "`", "?", ">", "<", ":", "/", "."];
 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// CHOOSE PASSWORD LENGTH
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to have? Please enter a number from 8 - 128.");
+
+  if (passwordLength != null , passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid response. Please enter a number 8 - 128.")
+  }
+
+ // CHOOSE PASSWORD CRITERIA
+ var passwordLower = confirm("Include lowercase?");
+ var passwordUpper = confirm("Include uppercase?");
+ var passwordNumber = confirm("Include numbers?");
+ var passwordSymbol = confirm("Include symbols?")
+   while(passwordUpper === false && passwordLower === false && passwordSymbol === false && passwordNumber === false) {
+     alert("You must choose at least one character type.");
+     var passwordLower = confirm("Include lowercase?");
+     var passwordUpper = confirm("Include uppercase?");
+     var passwordNumber = confirm("Include numbers?");
+     var passwordSymbol = confirm("Include symbols?")    
+ }
+
+ var randomPassword = "";
+
+
 }
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbols() {
-  const symbols = "!@#$%^&*()_+=-~`?><:/\|.,";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-console.log(getRandomSymbols());
 
 //Write password to the #password input
 function writePassword() {
